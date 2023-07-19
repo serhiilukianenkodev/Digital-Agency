559a1f9251be9401c1a36038d58fb774b3fa670e
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2023-07-19 23:37:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function removeChar(str) {
-  return str.slice(1, -1);
+function litres(time) {
+  return Math.floor(time * 0.5);
 }
 
-console.log(removeChar("Jsfvdfv"));
+litres(1.4);
+litres(6.7);
+litres(11.8);
